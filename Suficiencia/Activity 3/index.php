ada2a112708a8da60b55559892d185609f618128
Xann53 <128435023+Xann53@users.noreply.github.com>
2023-10-21 17:51:08 +0800

    Add files via upload

---
+++ Suficiencia/Activity 3/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <style>
+        div.mainContainer {
+            position: absolute;
+            top: 0;
+            right: 0;
+            height: 100%;
+            width: 100%;
+            overflow-y: scroll;
+        }
+        div.navbar {
+            display: flex;
+            position: relative;
+            height: 55px;
+            width: 100%;
+            background-color: #2FAF89;
+            align-items: center;
+            padding: 0px 30px;
+            flex-direction: row;
+        }
+        h1.title {
+            margin: 0px;
+            color: white;
+            font-size: 50px;
+            transition: 2s;
+        }
+        h1.title:hover {
+            margin: 0px;
+            color: white;
+            font-size: 60px;
+            transition: 0.2s;
+        }
+    </style>
+    <body lang="en" class="" style="height: auto; font-family: Helvetica">
+        <div class="mainContainer">
+            <div class="navbar">
+                <h1 class="title">MENU INPUT</h1>
+            </div>
+        </div>
+    </body>
+</html>
